3a384035b2117b09363fd62fcb1486316f1b0242
zeusintuivo <zeus@intuivo.com>
2021-10-25 21:03:17 +0200

    added fixes to take denied user and denied stash

--- update_bash_intuivo_cli.bash
+++ update_bash_intuivo_cli.bash
@@ -16,13 +16,14 @@
 # remember to restart the crontab everytime  this script chances to see changes take change in the crontab
 
 
-function updateSelf() {
+function update_bash_intuivo_cli() {
   local -i _err=0
   local -i _cron_running=1
   local _msg
-  local _log_file="${HOME}/update_bash_intuivo_cli.log"
-  local _lock_file="${HOME}/update_bash_intuivo_cli.lock"
-  local _lock_updated="${HOME}/updated_bash_intuivo_cli.lock"
+  local _home="${HOME}"
+  local _log_file="${_home}/update_bash_intuivo_cli.log"
+  local _lock_file="${_home}/update_bash_intuivo_cli.lock"
+  local _lock_updated="${_home}/updated_bash_intuivo_cli.lock"
   echo "" > "${_log_file}"
   if [ -e "${_lock_file}" ] ; then
   {
@@ -33,15 +34,15 @@ function updateSelf() {
   touch "${_lock_file}"
   local _log_path="/dev/fd/3"
   local -i _no_wall_broadcast_no_banner=1
-  function interrupt_updateSelf() {
+  function interrupt_update_bash_intuivo_cli() {
       echo "${0} ${FUNCNAME[0]} INTERRUPT"
       [ -e "${_lock_file}" ] && rm "${_lock_file}"
   }
-  function error_updateSelf() {
+  function error_update_bash_intuivo_cli() {
       echo "${0} ${FUNCNAME[0]} ERROR"
       [ -e "${_lock_file}" ] && rm "${_lock_file}"
   }
-    function exit_updateSelf() {
+    function exit_update_bash_intuivo_cli() {
       echo "${0} ${FUNCNAME[0]} EXIT"
       [ -e "${_lock_file}" ] && rm "${_lock_file}"
       if [ ${_cron_running} -ne 0 ] ; then
@@ -54,9 +55,9 @@ function updateSelf() {
       }
       fi
   }
-  trap interrupt_updateSelf INT
-  trap error_updateSelf ERR
-  trap exit_updateSelf EXIT
+  trap interrupt_update_bash_intuivo_cli INT
+  trap error_update_bash_intuivo_cli ERR
+  trap exit_update_bash_intuivo_cli EXIT
 # Ok using technique that env un crontab and env with user
 # different I observed different env
 # also calling the script from command line causes type -f to respond
@@ -200,7 +201,7 @@ fi
 #    }
 #  fi
 
-  local _target_folder="${HOME}/_/clis/bash_intuivo_cli"
+  local _target_folder="${_home}/_/clis/bash_intuivo_cli"
   # shellcheck disable=SC2164
   # shellcheck disable=SC2155
   local _msg=$(cd "${_target_folder}" 2>&1)
@@ -208,93 +209,209 @@ fi
   [ ${_err} -ne 0 ] && ( (log "ERROR while CD into \ntarget_folder: ${_target_folder}\nmsg:${_msg}" 2>&1) | tee -a "${_log_path}") && exit 1
   # shellcheck disable=SC2164
   cd "${_target_folder}"
-  # shellcheck disable=SC2155
-  local _current_branch=$(/usr/bin/git rev-parse --abbrev-ref HEAD 2>&1)
-  _err=$?
-  [ ${_err} -ne 0 ] && _current_branch="master"
-  # shellcheck disable=SC2155
-  local _target_branch="${_current_branch}"
-  _err=$?
-  # shellcheck disable=SC2155
-  local _git_remote_url=$(/usr/bin/git config --get remote.origin.url)
-  _err=$?
-  # shellcheck disable=SC2155
-  local _git_commit_hash_local=$(/usr/bin/git rev-parse "${_target_branch}")
-  _err=$?
-  # shellcheck disable=SC2155
-  local _git_commit_hash_remote=$(/usr/bin/git ls-remote $_git_remote_url "${_target_branch}" | awk '{ print $1}')
-  _err=$?
-  # shellcheck disable=SC2155
-  local _status_message="Self update ran on/at $(date '+%Y-%m-%d %H:%M:%S')"
-  _err=$?
-  local _output_message="${_status_message}, ${_target_branch} up to date, nothing to do"
-  _err=$?
-  local _update_message
+
+
+
   local -i _updated=0
   local -i _update_error=0
+  # shellcheck disable=SC2155
+  local _git_user=$(/usr/bin/git config --get user.name)
+  _err=$?
 
   (log INFO _no_wall_ "${_no_wall_broadcast_no_banner}" 2>&1) | tee -a "${_log_path}"
   (log INFO _log_file "${_log_file}" 2>&1) | tee -a "${_log_path}"
   (log INFO _log_path "${_log_path}" 2>&1) | tee -a "${_log_path}"
-  (log INFO _____home "${HOME}" 2>&1) | tee -a "${_log_path}"
+  (log INFO _____home "${_home}" 2>&1) | tee -a "${_log_path}"
+  (log INFO _git_user "${_git_user}" 2>&1) | tee -a "${_log_path}"
   (log INFO sudo_user "${SUDO_USER}" 2>&1) | tee -a "${_log_path}"
   (log INFO _____user "${USER}" 2>&1) | tee -a "${_log_path}"
-  (log INFO _____pwd "$(pwd)" 2>&1) | tee -a "${_log_path}"
-  (log INFO ___local "${_git_commit_hash_local}" 2>&1) | tee -a "${_log_path}"
-  (log INFO __remote "${_git_commit_hash_remote}" 2>&1) | tee -a "${_log_path}"
-  if [[ "${_git_commit_hash_local}" != "${_git_commit_hash_remote}" ]]; then
+  (log INFO ______pwd "$(pwd)" 2>&1) | tee -a "${_log_path}"
+
+  local _message_date="$(date '+%Y-%m-%d %H:%M:%S')"
+  local _message_did_nothing="update_bash_intuivo_cli nothing to do on/at ${_message_date}"
+  local      _message_ran_on="update_bash_intuivo_cli ran           on/at ${_message_date}"
+  local _output_message="${_message_did_nothing}"
+
+  function private_do_pull() {
+    # shellcheck disable=SC2155
+    local _target_branch="${1}"
+    _err=$?
+    # shellcheck disable=SC2155
+    local _git_remote_url=$(/usr/bin/git config --get remote.origin.url)
+    _err=$?
+    # shellcheck disable=SC2155
+    local _current_branch=$(/usr/bin/git rev-parse --abbrev-ref HEAD 2>&1)
+    _err=$?
+    [ ${_err} -ne 0 ] && _current_branch="master"
+    (log INFO cu_branch "${_current_branch}" 2>&1) | tee -a "${_log_path}"
+    (log INFO ta_branch "${_target_branch}" 2>&1) | tee -a "${_log_path}"
+    # shellcheck disable=SC2155
+    local _git_commit_hash_local=$(/usr/bin/git rev-parse "${_target_branch}")
+    _err=$?
+    # shellcheck disable=SC2155
+    local _git_commit_hash_remote=$(/usr/bin/git ls-remote "${_git_remote_url}" "${_target_branch}"  2>&1)
+    _err=$?
+    # echo "[$_err] ${_git_commit_hash_remote}"
+    # if [ ${_err} -ne 0 ]; then
+    # {
+      if [[ ${_git_commit_hash_remote} == *"Could not read from remote"* ]] ; then
+      {
+        #       git@github.com: Permission denied (publickey).
+        # fatal: Could not read from remote repository.
+
+        # Please make sure you have the correct access rights
+        # and the repository exists.
+        ( log ERROR "Permission denied for ${_git_user} for ${_git_remote_url} " 2>&1 ) | tee -a "${_log_path}"
+        _update_error=1
+         return $_update_error
+      }
+      fi
+    # }
+    # fi
+    _git_commit_hash_remote="$(echo -n "${_git_commit_hash_remote}" | awk '{ print $1}')"
+    _err=$?
+    # shellcheck disable=SC2155
+    local _status_message="${_message_ran_on}"
+    _err=$?
+    local _update_message=""
+    local _output_message="${_status_message}, ${_git_user} ${_target_branch} up to date, nothing to do"
+    (log INFO ___local "${_git_commit_hash_local}" 2>&1) | tee -a "${_log_path}"
+    (log INFO __remote "${_git_commit_hash_remote}" 2>&1) | tee -a "${_log_path}"
+    if [[ "${_git_commit_hash_local}" != "${_git_commit_hash_remote}" ]]; then
     {
       if [[ "${_current_branch}" != "${_target_branch}" ]]; then
+      {
+        # PULL TO ANOTHER ORIGIN TO ANOTHER BRANCH WHILE STAYING THIS BRANCH
+        # _update_message=$(/usr/bin/git fetch -f origin master:master 2>&1) # master to master sample
+        # _update_message=$(/usr/bin/git fetch -f origin main:main 2>&1) # master to master sample
+        _update_message=$(/usr/bin/git fetch -f origin "${_target_branch}":"${_target_branch}" 2>&1)
+        _err=$?
+        echo "[$_err] ${_update_message}"
+        if [ ${_err} -ne 0 ] || [[ "${_update_message}" == *"You have unstaged"* ]] ; then
         {
-          # PULL TO ANOTHER ORIGIN TO ANOTHER BRANCH WHILE STAYING THIS BRANCH
-          _update_message=$(/usr/bin/git fetch -f origin master:master 2>&1) # master to master sample
-          _update_message=$(/usr/bin/git fetch -f origin main:main 2>&1) # master to master sample
-          _update_message=$(/usr/bin/git fetch -f origin "${_target_branch}":"${_target_branch}" 2>&1)
+          # if ; then
+          # {
+            #  error: cannot pull with rebase: You have unstaged changes.
+            # please commit or stash them.
+            ( log ERROR "You have unstaged files for ${_target_branch} " 2>&1 ) | tee -a "${_log_path}"
+          # }
+          # fi
+          _update_error=1
+          return $_update_error
         }
-      else
+        else
         {
-          _update_message=$(/usr/bin/git pull -f 2>&1)
+          if [[ "${_update_message}" == *"Already up to date"* ]] ;  then
+          {
+            _updated=0
+          }
+          else
+          {
+            _output_message="${_update_message}, ${_status_message}, ${_target_branch} pulled"
+           ( log INFO "${_output_message}" 2>&1 ) | tee -a "${_log_path}"
+            _updated=1
+          }
+          fi
         }
-      fi
-      (echo remote "${_update_message}" 2>&1) | tee -a "${_log_path}"
+        fi
 
-      _err=$?
-      if [ ${_err} -ne 0 ]; then
-      {
-        _output_message="${_status_message}, Error occurred: [$_err] ${_update_message}"
-        _update_error=1
       }
       else
       {
-       if [[ "${_update_message}" == *"Already up to date"* ]] ;  then
-       {
-         _updated=0
-       }
-       else
-       {
-         _output_message="${_update_message}, ${_status_message}, ${_target_branch} pulled"
-         _updated=1
-       }
-       fi
+        _update_message=$(/usr/bin/git pull -f 2>&1)
+        echo "[$_err] ${_update_message}"
+        _err=$?
+        # (echo remote "${_update_message}" 2>&1) | tee -a "${_log_path}"
+        if [ ${_err} -ne 0 ] || [[ "${_update_message}" == *"You have unstaged"* ]]  ; then
+        {
+          ( log ERROR "You have unstaged files for ${_target_branch} " 2>&1 ) | tee -a "${_log_path}"
+          # _output_message="${_status_message}, ${_git_user} Error occurred: [$_err] ${_update_message}"
+          _update_error=1
+        }
+        else
+        {
+          if [[ "${_update_message}" == *"Already up to date"* ]] ;  then
+          {
+            _updated=0
+          }
+          else
+          {
+            _output_message="${_update_message}, ${_status_message}, ${_target_branch} pulled"
+           ( log INFO "${_output_message}" 2>&1 ) | tee -a "${_log_path}"
+            _updated=1
+          }
+          fi
+        }
+        fi
+
       }
       fi
+
     }
-  else
+    else
     {
       _output_message="${_status_message}, ${_target_branch} already up to date (nothing to do)"
+     ( log INFO "${_output_message}" 2>&1 ) | tee -a "${_log_path}"
     }
-  fi # end if git_commit_hash_
+    fi # end if git_commit_hash_
+    # ( log INFO "${_output_message}" 2>&1 ) | tee -a "${_log_path}"
 
-  ( log INFO "${_output_message}" 2>&1 ) | tee -a "${_log_path}"
+    # Run external cron file
+    if [[ -e "${_target_folder}/update_cron_bash_intuivo_cli.bash" ]] ; then
+    {
+      (log INFO filecron "found ${_target_folder}/update_cron_bash_intuivo_cli.bash" 2>&1) | tee -a "${_log_path}"
+      ( "${_target_folder}/update_cron_bash_intuivo_cli.bash" 2>&1 ) | tee -a "${_log_path}"
+    }
+    else
+    {
+      (log INFO filecron "not found ${_target_folder}/update_cron_bash_intuivo_cli.bash" 2>&1) | tee -a "${_log_path}"
+    }
+    fi
 
-  # Run external cron file
-  ( "${_target_folder}/update_cron_bash_intuivo_cli.bash" 2>&1 ) | tee -a "${_log_path}"
+    if [ ${_update_error} -ne 0 ] ; then
+    {
+          (log ERROR "${_output_message}" 2>&1) | tee -a "${_log_path}"
+    }
+    fi
 
-  if [ ${_update_error} -ne 0 ] ; then
+  } # end private_do_pull
+  if is_in_local "master" ; then
   {
-        (log ERROR "${_output_message}" 2>&1) | tee -a "${_log_path}"
+    private_do_pull "master"
   }
   fi
+  if is_in_local "main" ; then
+  {
+    private_do_pull "main"
+  }
+  fi
+  # shellcheck disable=SC2155
+  local _current_branch=$(/usr/bin/git rev-parse --abbrev-ref HEAD 2>&1)
+  _err=$?
+  if [[ "${_current_branch}" != "master" ]] && [[ "${_current_branch}" != "main" ]] ; then
+  {
+    private_do_pull "${_current_branch}"
+  }
+  fi
+
+
+  [ -e "${_lock_file}" ] && rm "${_lock_file}"
+
+    if [ ${_updated} -ne 0 ] ; then
+    {
+      touch "${_lock_updated}"
+      if [ ${_no_wall_broadcast_no_banner} -ne 0 ] ; then
+      {
+         wall <<<"${_output_message}"
+      }
+      else
+      {
+         wall <<< "${_output_message}" # wall: --nobanner is available only for root
+      }
+      fi
+    }
+    fi
+
 
   # restore stdout and stderr
   if [ ${_no_wall_broadcast_no_banner} -ne 0 ] ; then
@@ -306,22 +423,6 @@ fi
     exec 1>&3  # How to redirect output  REF: https://stackoverflow.com/questions/314675/how-to-redirect-output-of-an-entire-shell-script-within-the-script-itself
   }
   fi
-  if [ ${_updated} -ne 0 ] ; then
-  {
-    touch "${_lock_updated}"
-    if [ ${_no_wall_broadcast_no_banner} -ne 0 ] ; then
-    {
-       wall <<<"${_output_message}"
-    }
-    else
-    {
-       wall <<< "${_output_message}" # wall: --nobanner is available only for root
-    }
-    fi
-  }
-  fi
-
-  [ -e "${_lock_file}" ] && rm "${_lock_file}"
 
   if [ ${_update_error} -ne 0 ] ; then
   {
@@ -331,7 +432,39 @@ fi
 
   exit 0
 
-} # end updateSelf
+} # end update_bash_intuivo_cli
+
+
+function is_in_local() {
+  # Local:
+  # https://stackoverflow.com/questions/21151178/shell-script-to-check-if-specified-git-branch-exists
+  # test if the branch is in the local repository.
+  # if is_in_local "master" ; then
+  # {
+  #   echo "brach master exists locallly"
+  # }
+  # fi
+  local branch="${1}"
+  local existed_in_local=$(/usr/bin/git branch --list "${branch}")
+  [[ -z ${existed_in_local} ]] && return 1
+  return 0
+} # end is_in_local
+
+function is_in_remote() {
+  # Remote:
+  # Ref: https://stackoverflow.com/questions/8223906/how-to-check-if-remote-branch-exists-on-a-given-remote-repository
+  # test if the branch is in the remote repository.
+  # Usage:
+  # if is_in_remote "master" ; then
+  # {
+  #   echo "brach master exists remotely"
+  # }
+  # fi
+  local branch="${1}"
+  local existed_in_remote=$(/usr/bin/git ls-remote --heads origin "${branch}")
+  [[ -z ${existed_in_remote} ]] && return 1
+  return 0
+} # end is_in_remote
 
 function doLog() {
   # Sample use: REF: https://stackoverflow.com/questions/18460186/writing-outputs-to-log-file-and-console
@@ -370,11 +503,11 @@ function doLog() {
 
 function log() {
   #  usage:
-  # doLog "INFO some info message"
-  # doLog "DEBUG some debug message"
-  # doLog "WARN some warning message"
-  # doLog "ERROR some really ERROR message"
-  # doLog "FATAL some really fatal message"
+  # log "INFO some info message"
+  # log "DEBUG some debug message"
+  # log "WARN some warning message"
+  # log "ERROR some really ERROR message"
+  # log "FATAL some really fatal message"
   # shellcheck disable=SC2155
   local type_of_msg=$(echo "${@}" | cut -d" " -f1)
   # shellcheck disable=SC2155
@@ -383,4 +516,4 @@ function log() {
 
 } # end log
 
-updateSelf
+update_bash_intuivo_cli
